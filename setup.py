68df87765999543aa3adfa1da3dc47520370e94d
Noam Bernstein <noam.bernstein@nrl.navy.mil>
2021-11-19 16:35:45 -0500

    Use find_packages to get subdirectories, exclude tests

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     name="universalSOAP",
     version="0.1.0b",
-    packages=["universalSOAP"],
+    packages=setuptools.find_packages(exclude=["tests"]),
     entry_points="""
     [console_scripts]
     universalSOAP_descriptors=universalSOAP.cli.write_descriptor_quantities:main
